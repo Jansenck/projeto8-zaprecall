863191035c09c8ed659d3129e2df9bf3c04120ce
Jansenck <caik11@hotmail.com>
2022-07-25 18:05:46 -0300

    commit

--- projeto8-zaprecall/src/components/App.js
+++ projeto8-zaprecall/src/components/App.js
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { React, useState } from "react";
 
 import Flashcards from "./Flashcards";
 
 import logo from "../assets/img/logo.png";
+import party from "../assets/img/party.png";
+import sad from "../assets/img/sad.png";
 
 export default function App(){
 
@@ -12,60 +14,67 @@ export default function App(){
     const flashcards = [
 
         {
-            name: "pergunta 1",
+            id:0,
             question: "O que é JSX?",
             answer: "Uma extensão de linguagem do JavaScript",
             showQuestion: false,
-            showAnswer: false
+            showAnswer: false,
+            answered: false
         },
         {
-            name: "pergunta 2",
+            id:1,
             question: "O React é __",
             answer: "uma biblioteca JavaScript para construção de interfaces.",
             showQuestion: false,
-            showAnswer: false
+            showAnswer: false,
         },
         {
-            name: "pergunta 3",
+            id:2,
             question: "Componentes devem iniciar com __",
             answer: " letra maiúscula",
             showQuestion: false,
-            showAnswer: false
+            showAnswer: false,
+            answered: false
         },
         {
-            name: "pergunta 4",
+            id:3,
             question: "Podemos colocar __ dentro do JSX",
             answer: "expressões",
             showQuestion: false,
-            showAnswer: false
+            showAnswer: false,
+            answered: false
         },
         {
-            name: "pergunta 5",
+            id:4,
             question: " O ReactDOM nos ajuda __ ",
             answer: " interagindo com a DOM para colocar componentes React na mesma",
             showQuestion: false,
-            showAnswer: false
+            showAnswer: false,
+            answered: false
         },
         {
-            name: "pergunta 6",
+            id:5,
             question: "Usamos o npm para __ ",
             answer: "gerenciar os pacotes necessários e suas dependências",
             showQuestion: false,
-            showAnswer: false
+            showAnswer: false,
+            answered: false
         },
         {
-            name: "pergunta 7",
+            id:6,
             question: "Usamos props para __",
             answer: "passar diferentes informações para componentes ",
             showQuestion: false,
-            showAnswer: false
+            showAnswer: false,
+            answered: false
         },
         {
-            name: "pergunta 8",
+            id:7,
             question: "Usamos estado (state) para __ ",
             answer: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente",
             showQuestion: false,
-            showAnswer: false
+            showAnswer: false,
+            answered: false
         }
     ];
 
@@ -76,6 +85,42 @@ export default function App(){
     } );
 
     const [flashcardsData, setFlashcardsData] = useState(flashcards);
+
+    function Icons(){
+        return(userAnswers.map((icon, index)=> {
+            
+            return(
+                <ion-icon name={icon.icon} style={{color: icon.color}} ket={index}></ion-icon>
+            );
+        }))
+    }
+
+    function Feedback(){
+
+        const feedback = userAnswers.every((verify, i, arr) => {
+
+            return verify.feedbackAnswer === true
+        })
+
+        return(
+            (feedback)?
+            <>
+                <div className="emoticon">
+                    <img src={party} alt={party}/>
+                    <p>Parabéns!</p>
+                </div>
+                <p>Você não esqueceu de<br/> nenhum flashcard!</p>
+            </>
+            :
+            <>
+                <div className="emoticon">
+                    <img src={sad} alt={sad}/>
+                    <p>Puts...</p>
+                </div>
+                <p>Ainda faltam alguns...<br/> Mas não desanime!</p>                                
+            </>
+        );
+    }
 
     return(
             (start)?
@@ -94,8 +139,21 @@ export default function App(){
                     setUserAnswers={setUserAnswers}
                 />
 
-                <footer className="progressBar">
-                    <p>{userAnswers.length}/{flashcards.length} CONCLUÍDOS</p>
+                <footer className={`progressBar ${userAnswers.length === 8? "expand" : ""}`} >
+                    <div className="feedback">
+                        {
+                            (userAnswers.length === 8)?
+                                <Feedback userAnswers={userAnswers} />
+                            :
+                                <></>
+                        }
+                        <div className="questionsAnswered">
+                            <p>{userAnswers.length}/{flashcards.length} CONCLUÍDOS</p>
+                            <div>
+                                <Icons/>
+                            </div>
+                        </div>
+                    </div>
                 </footer>
             </div>
 
--- projeto8-zaprecall/src/components/Flashcards.js
+++ projeto8-zaprecall/src/components/Flashcards.js
@@ -1,93 +1,6 @@
+import Card from "./Card.js";
 
-import arrow from "../assets/img/setinha.png"
-import play from "../assets/img/play.png"
-
-function card(action, index, flashcardsData, setFlashcardsData){
-    
-    
-        (action === "show-question")?
-            flashcardsData[index] = {...flashcardsData[index], showQuestion: true}
-        :
-            flashcardsData[index] = {...flashcardsData[index], showAnswer: true}
-    
-    const updateCardsData = [...flashcardsData];
-    setFlashcardsData(updateCardsData);
-}
-
-function Card(props){
-
-    const {index, flashcard, flashcardsData, setFlashcardsData} = props;
-
-    return(
-        <>
-            <Text 
-            index={index}
-            flashcard={flashcard}
-            flashcardsData={flashcardsData}
-            setFlashcardsData={setFlashcardsData}
-            />
-            {
-                (flashcard.showAnswer)?
-                    <Buttons/>
-                :
-                    <Icon 
-                    index={index}
-                    flashcard={flashcard}
-                    flashcardsData={flashcardsData}
-                    setFlashcardsData={setFlashcardsData}
-                    />
-            }
-        </>
-    );
-}
-
-function Question(props){
-    const {flashcard} = props;
-    return(
-        (flashcard.showAnswer)?
-            <p>{flashcard.answer}</p>
-            :
-            <p>{flashcard.question}</p>
-    );
-}
-
-function Text(props){
-
-    const {flashcard} = props;
-
-    return(
-        
-        (flashcard.showQuestion)?
-                <Question flashcard={flashcard}/>
-            :
-                <p>{flashcard.name}</p>
-    );
-}
-
-function Buttons(){
-
-    return(
-        <div className="buttons">
-            <button style={{background:"#FF3030"}}>Não lembrei</button>
-            <button style={{background:"#FF922E"}}>Quase não lembrei</button>
-            <button style={{background:"#2FBE34"}}>Zap!</button>
-        </div>
-    );
-}
-
-function Icon(props){
-
-    const {index, flashcard, flashcardsData, setFlashcardsData} = props;
-
-    return(
-        (flashcard.showQuestion)?
-            <img src={arrow} alt={arrow} onClick={() => card("show-answer",index, flashcardsData, setFlashcardsData)}/>
-        :
-            <img src={play} alt={play} onClick={() => card("show-question", index, flashcardsData, setFlashcardsData)}/>   
-    );
-}
-
-export default function Flashcards({flashcardsData, setFlashcardsData, setUserAnswers}){
+export default function Flashcards({flashcardsData, setFlashcardsData, userAnswers, setUserAnswers}){
 
     return(
         <div className="deck">
@@ -99,6 +12,7 @@ export default function Flashcards({flashcardsData, setFlashcardsData, setUserAn
                             flashcard={flashcard} 
                             flashcardsData={flashcardsData} 
                             setFlashcardsData={setFlashcardsData}
+                            userAnswers={userAnswers}
                             setUserAnswers={setUserAnswers}
                         />
                     </div>
