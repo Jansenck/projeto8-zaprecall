c986fbd3f549933dbd9cdbd934b0b02cb02a4acf
Jansenck <caik11@hotmail.com>
2022-07-25 21:23:38 -0300

    Deployment

--- projeto8-zaprecall/src/index.js
+++ projeto8-zaprecall/src/index.js
@@ -1,7 +1,7 @@
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./components/App";
 
 const container = document.querySelector('.root');
-//const root = ReactDOM.createRoot(container);
+const root = ReactDOM.createRoot(container);
 
-ReactDOM.render(<App/>, container);
+root.render(<App/>);
